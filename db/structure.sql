49c672cb23315211fdd23e44e669ffbf4ffe8ef5
Jack Schultz <jackschultz23@gmail.com>
2019-10-25 09:11:54 -0500

    dropping again to structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -31,6 +31,42 @@ COMMENT ON EXTENSION unaccent IS 'text search dictionary that removes accents';
 
 
 --
+-- Name: avg_prev_pp36(integer, date, integer); Type: FUNCTION; Schema: public; Owner: jackschultz
+--
+
+CREATE FUNCTION public.avg_prev_pp36(qpid integer, before_date date, limit_back integer) RETURNS numeric
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+   RETURN 
+   
+   	(
+		SELECT
+			round(avg(slp.fdpp36), 2)
+		FROM
+			stat_line_points slp
+		WHERE
+			slp.slid in(
+				SELECT
+					slid FROM stat_line_points slp2
+				WHERE
+					season = slp.season
+					AND slp2."date" < before_date
+					AND slp2.fdpp36 IS NOT NULL
+					AND slp2.player_id = qpid
+				ORDER BY
+					date DESC
+				LIMIT limit_back)
+				and slp.player_id=qpid
+		GROUP BY
+			slp.player_id);
+   
+END; $$;
+
+
+ALTER FUNCTION public.avg_prev_pp36(qpid integer, before_date date, limit_back integer) OWNER TO jackschultz;
+
+--
 -- Name: clean_differences(text); Type: FUNCTION; Schema: public; Owner: jackschultz
 --
 
@@ -205,6 +241,117 @@ CREATE TABLE public.games (
 ALTER TABLE public.games OWNER TO nbauser;
 
 --
+-- Name: stat_lines; Type: TABLE; Schema: public; Owner: nbauser
+--
+
+CREATE TABLE public.stat_lines (
+    id integer NOT NULL,
+    player_id integer NOT NULL,
+    team_id integer NOT NULL,
+    game_id integer NOT NULL,
+    dk_positions character varying(15),
+    fd_positions character varying(15),
+    dk_salary integer,
+    fd_salary integer,
+    dk_points numeric,
+    fd_points numeric,
+    stats jsonb,
+    minutes numeric DEFAULT 0.0,
+    active boolean DEFAULT true
+);
+
+
+ALTER TABLE public.stat_lines OWNER TO nbauser;
+
+--
+-- Name: teams; Type: TABLE; Schema: public; Owner: nbauser
+--
+
+CREATE TABLE public.teams (
+    id integer NOT NULL,
+    name character varying(50) NOT NULL,
+    abbrv character varying(10) NOT NULL,
+    rg_abbrv character varying(10) NOT NULL,
+    br_abbrv character varying(10)
+);
+
+
+ALTER TABLE public.teams OWNER TO nbauser;
+
+--
+-- Name: stat_line_points; Type: VIEW; Schema: public; Owner: nbauser
+--
+
+CREATE VIEW public.stat_line_points AS
+ SELECT p.br_name AS name,
+    t.abbrv,
+    g.date,
+    round(sl.minutes, 2) AS minutes,
+    sl.dk_positions,
+    sl.dk_salary,
+    round(sl.dk_points, 2) AS dk_points,
+    round((sl.dk_points * (36.0 / NULLIF(sl.minutes, (0)::numeric))), 2) AS dkpp36,
+    sl.fd_salary,
+    sl.fd_positions,
+    round(sl.fd_points, 2) AS fd_points,
+    round((sl.fd_points * (36.0 / NULLIF(sl.minutes, (0)::numeric))), 2) AS fdpp36,
+    p.id AS player_id,
+    sl.id AS slid,
+    g.season
+   FROM public.stat_lines sl,
+    public.games g,
+    public.players p,
+    public.teams t
+  WHERE ((sl.game_id = g.id) AND (sl.player_id = p.id) AND (sl.team_id = t.id));
+
+
+ALTER TABLE public.stat_line_points OWNER TO nbauser;
+
+--
+-- Name: avg_dk_sals; Type: MATERIALIZED VIEW; Schema: public; Owner: jackschultz
+--
+
+CREATE MATERIALIZED VIEW public.avg_dk_sals AS
+ SELECT stat_line_points.dk_salary AS sal,
+    round(avg(stat_line_points.dk_points), 2) AS aver
+   FROM public.stat_line_points
+  WHERE ((stat_line_points.minutes > (0)::numeric) AND (stat_line_points.dk_salary IS NOT NULL))
+  GROUP BY stat_line_points.dk_salary
+  WITH NO DATA;
+
+
+ALTER TABLE public.avg_dk_sals OWNER TO jackschultz;
+
+--
+-- Name: avg_fd_sals; Type: VIEW; Schema: public; Owner: jackschultz
+--
+
+CREATE VIEW public.avg_fd_sals AS
+ SELECT stat_line_points.fd_salary AS sal,
+    round(avg(stat_line_points.fd_points), 2) AS aver
+   FROM public.stat_line_points
+  WHERE ((stat_line_points.minutes > (0)::numeric) AND (stat_line_points.fd_salary IS NOT NULL))
+  GROUP BY stat_line_points.fd_salary;
+
+
+ALTER TABLE public.avg_fd_sals OWNER TO jackschultz;
+
+--
+-- Name: avg_fd_sals_mat; Type: MATERIALIZED VIEW; Schema: public; Owner: jackschultz
+--
+
+CREATE MATERIALIZED VIEW public.avg_fd_sals_mat AS
+ SELECT stat_line_points.fd_salary AS sal,
+    round(avg(stat_line_points.fd_points), 2) AS aver
+   FROM public.stat_line_points
+  WHERE ((stat_line_points.minutes > (0)::numeric) AND (stat_line_points.fd_salary IS NOT NULL))
+  GROUP BY stat_line_points.fd_salary
+  WITH NO DATA;
+
+
+ALTER TABLE public.avg_fd_sals_mat OWNER TO jackschultz;
+
+--
 -- Name: games_id_seq; Type: SEQUENCE; Schema: public; Owner: nbauser
 --
 
@@ -249,22 +396,25 @@ ALTER SEQUENCE public.players_id_seq OWNED BY public.players.id;
 
 
 --
--- Name: projections; Type: TABLE; Schema: public; Owner: jackschultz
+-- Name: projections; Type: TABLE; Schema: public; Owner: nbauser
 --
 
 CREATE TABLE public.projections (
     id integer NOT NULL,
     stat_line_id integer,
     source character varying(50),
-    "all" jsonb,
-    minutes numeric DEFAULT 0.0
+    bulk jsonb,
+    minutes numeric DEFAULT 0.0,
+    dk_points numeric,
+    fd_points numeric,
+    active boolean DEFAULT true
 );
 
 
-ALTER TABLE public.projections OWNER TO jackschultz;
+ALTER TABLE public.projections OWNER TO nbauser;
 
 --
--- Name: projections_id_seq; Type: SEQUENCE; Schema: public; Owner: jackschultz
+-- Name: projections_id_seq; Type: SEQUENCE; Schema: public; Owner: nbauser
 --
 
 CREATE SEQUENCE public.projections_id_seq
@@ -276,71 +426,44 @@ CREATE SEQUENCE public.projections_id_seq
     CACHE 1;
 
 
-ALTER TABLE public.projections_id_seq OWNER TO jackschultz;
+ALTER TABLE public.projections_id_seq OWNER TO nbauser;
 
 --
--- Name: projections_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: jackschultz
+-- Name: projections_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: nbauser
 --
 
 ALTER SEQUENCE public.projections_id_seq OWNED BY public.projections.id;
 
 
 --
--- Name: stat_lines; Type: TABLE; Schema: public; Owner: nbauser
+-- Name: slpp; Type: VIEW; Schema: public; Owner: jackschultz
 --
 
-CREATE TABLE public.stat_lines (
-    id integer NOT NULL,
-    player_id integer NOT NULL,
-    team_id integer NOT NULL,
-    game_id integer NOT NULL,
-    dk_positions character varying(15),
-    fd_positions character varying(15),
-    dk_salary integer,
-    fd_salary integer,
-    dk_points numeric,
-    fd_points numeric,
-    stats jsonb,
-    minutes numeric DEFAULT 0.0,
-    active boolean DEFAULT true
-);
-
-
-ALTER TABLE public.stat_lines OWNER TO nbauser;
-
---
--- Name: teams; Type: TABLE; Schema: public; Owner: nbauser
---
-
-CREATE TABLE public.teams (
-    id integer NOT NULL,
-    name character varying(50) NOT NULL,
-    abbrv character varying(10) NOT NULL,
-    rg_abbrv character varying(10) NOT NULL,
-    br_abbrv character varying(10)
-);
-
-
-ALTER TABLE public.teams OWNER TO nbauser;
-
---
--- Name: stat_line_points; Type: VIEW; Schema: public; Owner: jackschultz
---
-
-CREATE VIEW public.stat_line_points AS
- SELECT p.br_name,
+CREATE VIEW public.slpp AS
+ SELECT p.br_name AS name,
     t.abbrv,
     g.date,
     round(sl.minutes, 2) AS minutes,
     sl.dk_positions,
     sl.dk_salary,
-    sl.dk_points,
+    round(sl.dk_points, 2) AS dk_points,
     round((sl.dk_points * (36.0 / NULLIF(sl.minutes, (0)::numeric))), 2) AS dkpp36,
     sl.fd_salary,
     sl.fd_positions,
-    sl.fd_points,
+    round(sl.fd_points, 2) AS fd_points,
     round((sl.fd_points * (36.0 / NULLIF(sl.minutes, (0)::numeric))), 2) AS fdpp36,
-    p.id AS player_id
+    ( SELECT round(avg(round((sl2.fd_points * (36.0 / NULLIF(sl2.minutes, (0)::numeric))), 2)), 2) AS round
+           FROM public.stat_lines sl2
+          WHERE (sl2.id IN ( SELECT sl3.id
+                   FROM public.stat_lines sl3,
+                    public.games g3
+                  WHERE ((sl3.game_id = g3.id) AND (g3.date < g.date) AND (sl3.fd_points IS NOT NULL) AND (sl3.player_id = sl.player_id))
+                  ORDER BY g3.date DESC
+                 LIMIT 2))
+          GROUP BY sl2.player_id) AS avgfdpp36,
+    p.id AS player_id,
+    sl.id AS slid,
+    g.season
    FROM public.stat_lines sl,
     public.games g,
     public.players p,
@@ -348,7 +471,7 @@ CREATE VIEW public.stat_line_points AS
   WHERE ((sl.game_id = g.id) AND (sl.player_id = p.id) AND (sl.team_id = t.id));
 
 
-ALTER TABLE public.stat_line_points OWNER TO jackschultz;
+ALTER TABLE public.slpp OWNER TO jackschultz;
 
 --
 -- Name: stat_lines_id_seq; Type: SEQUENCE; Schema: public; Owner: nbauser
@@ -409,7 +532,7 @@ ALTER TABLE ONLY public.players ALTER COLUMN id SET DEFAULT nextval('public.play
 
 
 --
--- Name: projections id; Type: DEFAULT; Schema: public; Owner: jackschultz
+-- Name: projections id; Type: DEFAULT; Schema: public; Owner: nbauser
 --
 
 ALTER TABLE ONLY public.projections ALTER COLUMN id SET DEFAULT nextval('public.projections_id_seq'::regclass);
@@ -462,11 +585,19 @@ ALTER TABLE ONLY public.players
 
 
 --
--- Name: projections projections_pkey; Type: CONSTRAINT; Schema: public; Owner: jackschultz
+-- Name: projections projections_pkey; Type: CONSTRAINT; Schema: public; Owner: nbauser
 --
 
 ALTER TABLE ONLY public.projections
     ADD CONSTRAINT projections_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: projections projections_stat_line_id_source_key; Type: CONSTRAINT; Schema: public; Owner: nbauser
+--
+
+ALTER TABLE ONLY public.projections
+    ADD CONSTRAINT projections_stat_line_id_source_key UNIQUE (stat_line_id, source);
 
 
 --
@@ -534,10 +665,52 @@ ALTER TABLE ONLY public.teams
 
 
 --
+-- Name: games_date_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX games_date_idx ON public.games USING btree (date);
+
+
+--
 -- Name: players_br_name_idx; Type: INDEX; Schema: public; Owner: nbauser
 --
 
 CREATE INDEX players_br_name_idx ON public.players USING btree (br_name);
+
+
+--
+-- Name: stat_lines_active_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_active_idx ON public.stat_lines USING btree (active);
+
+
+--
+-- Name: stat_lines_dk_points_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_dk_points_idx ON public.stat_lines USING btree (dk_points);
+
+
+--
+-- Name: stat_lines_fd_points_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_fd_points_idx ON public.stat_lines USING btree (fd_points);
+
+
+--
+-- Name: stat_lines_game_id_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_game_id_idx ON public.stat_lines USING btree (game_id);
+
+
+--
+-- Name: stat_lines_game_id_player_id_team_id_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_game_id_player_id_team_id_idx ON public.stat_lines USING btree (game_id, player_id, team_id);
 
 
 --
@@ -552,6 +725,13 @@ CREATE INDEX stat_lines_minutes_idx ON public.stat_lines USING btree (minutes);
 --
 
 CREATE INDEX stat_lines_player_id_idx ON public.stat_lines USING btree (player_id);
+
+
+--
+-- Name: stat_lines_team_id_idx; Type: INDEX; Schema: public; Owner: nbauser
+--
+
+CREATE INDEX stat_lines_team_id_idx ON public.stat_lines USING btree (team_id);
 
 
 --
@@ -571,7 +751,7 @@ ALTER TABLE ONLY public.games
 
 
 --
--- Name: projections projections_stat_line_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: jackschultz
+-- Name: projections projections_stat_line_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: nbauser
 --
 
 ALTER TABLE ONLY public.projections
